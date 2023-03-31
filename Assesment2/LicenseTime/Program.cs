0b43b5c9cf59b9653bf0fd10c89dc96b40838c7b
theakshai <akshaiakshai35@gmail.com>
2023-03-31 12:21:43 +0100

    Added Assesment2

---
+++ Assesment2/LicenseTime/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace License;
+class TimeAllocation
+{
+  protected static string Name;
+  protected static int Agents;
+  protected static string NameList;
+
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine("Enter Your Name: ");
+      Name = Console.ReadLine();
+      
+
+        Console.WriteLine("Enter the number of Agents Available: ");
+        Agents = Convert.ToInt32(Console.ReadLine());
+
+    
+
+      Console.WriteLine("Enter the Other People names with separated spaces: ");
+      NameList = Console.ReadLine();
+      int minutes = License(Name, Agents, NameList);
+      Console.WriteLine(minutes);
+    }
+
+    static int License(string name, int agents, string nameList)
+    {
+      string[] names = nameList.Split(' ');
+      List<string> AllWaitingPeople = new List<string>(); 
+      foreach(string s in names){
+        AllWaitingPeople.Add(s);
+      }
+
+      AllWaitingPeople.Add(name);
+      AllWaitingPeople.Sort();
+      int PeopleAhead = AllWaitingPeople.FindIndex( s => s.StartsWith($"{name}"));
+      int numAgentsWorking = Math.Min(agents, PeopleAhead);
+      int minutes = PeopleAhead * 20 / numAgentsWorking;
+
+      return minutes;
+    }
+}
---
+++ Assesment2/NumberPlate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberPlate;
+
+public class NumberPlateGroupingService{
+
+  protected static string NumberPlateString;
+  protected static int GroupLimit;
+
+  static void Main(String[] args){
+    GettingData();
+    Console.WriteLine(GroupingString(NumberPlateString, GroupLimit));
+  }
+
+  static void GettingData(){
+
+    Console.WriteLine("Enter the Number Plate String:");
+    NumberPlateString = Console.ReadLine();
+
+
+      Console.WriteLine("Enter the Group Limit to be Seperated:");
+      GroupLimit = Convert.ToInt32(Console.ReadLine());
+
+  }
+
+  static string GroupingString(string numberPlateString, int groupLimit){
+    string SanitizingString = new string(numberPlateString.Where(c => char.IsLetterOrDigit(c)).ToArray()).ToUpper();
+
+
+    List<string> ModifiedString = new List<string>();
+    int i = SanitizingString.Length;
+    while (i > 0) {
+      int Length = Math.Min(groupLimit, i);
+      ModifiedString.Add(SanitizingString.Substring(i - Length,Length));
+      i -= Length;
+    }
+    ModifiedString.Reverse();
+    
+    return string.Join("-", ModifiedString);
+  } 
+}
